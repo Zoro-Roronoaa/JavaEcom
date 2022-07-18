66823778b7c327d0dc289f9fb2defac43e403a1c
Zoro-Roronoaa <105601736+Zoro-Roronoaa@users.noreply.github.com>
2022-07-18 12:55:05 -0700

    a

--- E-commerce/src/com/MAINM/Admin.java
+++ E-commerce/src/com/MAINM/Admin.java
@@ -10,63 +10,95 @@ import java.util.Scanner;
 import com.databases.ConnectionUtility;
 
 public class Admin {
+//creating new class Admin 
 	
-	
+	 
+	                 public void UserHistory() {                 //Method created which is use to select the userHistoty table from database Ecommerce
+
+		 ConnectionUtility obj= new ConnectionUtility();       //Accessing connection method from outside
+		
+		   PreparedStatement ps=null;
+		
+		try {
+			
+			       Connection con=obj.getConnection();
+			ps = con.prepareStatement("select * from UserHistory ");        //select query in prepareStatement object
+
+			ResultSet rs = ps.executeQuery();       //selecting Resultset which is use to print the table content
+
+			while (rs.next()) {
+
+				System.out.println("User_id=" + rs.getInt(1));
+				System.out.println("Product_id =" + rs.getInt(2));
+				System.out.println("Product_Name=" + rs.getString(3));   //printing the satatements using while loop
+				System.out.println("Quantity=" + rs.getInt(4));
+                                System.out.println("------------------------------------------------------------------");
+			}
+
+			con.close();
+			ps.close();             
+			rs.close();
+		} catch (Exception e) {              //catch the exception using try catch block
+			e.printStackTrace();
+		}
+	}
+
 	     
-	  public void ProductDetails()  {
-		  ConnectionUtility obj= new ConnectionUtility();
+	  public void ProductDetails()  {    //Method created which is use to select the Product table from database Ecommerce
+
+        ConnectionUtility obj= new ConnectionUtility();    //Accessing connection method from outside
 		
 		PreparedStatement ps=null;
 		
 		try {
 			
 			Connection con=obj.getConnection();
-	      ps=con.prepareStatement("select * from products ");
+	             ps=con.prepareStatement("select * from products ");  //select query in prepareStatement object
 
-		ResultSet rs=ps.executeQuery();
+		ResultSet rs=ps.executeQuery();         //selecting Resultset which is use to print the table content
 		
 		while(rs.next()) {
 			
 			System.out.println("Product_id="+rs.getInt(1));
 			System.out.println("Descrpition ="+rs.getString(2));
 			System.out.println("Price="+rs.getInt(3));
-			System.out.println("Product_Name="+rs.getString(4));
+			System.out.println("Product_Name="+rs.getString(4));      //printing the satatements using while loop
 			System.out.println("Quanity="+rs.getInt(5));
 			System.out.println("------------------------------------------------------------------");
 		}
 		con.close();
-		ps.close();
+		ps.close();        
 		rs.close();
 
 		}
 		catch(Exception e) {
-			e.printStackTrace();
+			e.printStackTrace();           //catch the exception using try catch block
 			}
 	   }
 	  
-	  public void UseRegistration() {
+	  public void UseRegistration() {              //Method created which is use to select the UserRegistration table from database Ecommerce
+
 		  
-			Connection con=null;
-			PreparedStatement ps=null;
+			ConnectionUtility obj= new ConnectionUtility();     //Accessing connection method from outside
+		
+		          PreparedStatement ps=null;
 			
 			try {
 				
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("Step1");
-			String url="jdbc:mysql://localhost:3306/ECommerce";
-			String username="root";
-			String password="MySql@123";
-		    con=DriverManager.getConnection(url, username, password);
-		      ps=con.prepareStatement("select * from UserRegistration");
+			Connection con=obj.getConnection();
 
-			ResultSet rs=ps.executeQuery();
+		      ps=con.prepareStatement("select * from UserRegistration");     //select query in prepareStatement object
+
+			ResultSet rs=ps.executeQuery();                                //selecting Resultset which is use to print the table content
+
 			
 			while(rs.next()) {
 				System.out.println("UserId="+rs.getInt(1));
 				System.out.println("UserFirstName ="+rs.getString(2));
 				System.out.println("UserLastName="+rs.getString(3));
-                System.out.println("UserContactNo="+rs.getLong(4));
-                System.out.println("UserCity="+rs.getString(5));
+                                 System.out.println("UserContactNo="+rs.getLong(4));            //printing the satatements using while loop
+                                 System.out.println("UserCity="+rs.getString(5));
+                         System.out.println("------------------------------------------------------------------");
 				
 			}
 			
@@ -75,39 +107,51 @@ public class Admin {
 			rs.close();
 
 		   }
-			catch(Exception e) {
+			catch(Exception e) {                             //catch the exception using try catch block
 				e.printStackTrace();
 				}
 		   }
 		  
 		  
 	  
-	  public void GetDatabase()  {
+	  public void GetDatabase()  {              //GateDatabase method created for Accessing the above created methods
+
+		Admin admin = new Admin();
+
+		Scanner sc = new Scanner(System.in);   //Scanner Class is used to take input 
+
+		System.out.println( "Enter the Required Details ,1:ProductDeatils 2:UserRegsistration 3: UserHistory:1 or 2, or  3");//taking input in the form of 1,2,3.
+		int data = sc.nextInt();
+
+
+
+		int a = 1;
+		int b = 2;
+		int c = 3;
+
+		switch (data) {     //switch statement is used to selct the methods as per the input.
+		case 1:
+			if (a == data) {
+
+				admin.ProductDetails();
+				break;
+
+			}
+		case 2:
+			if (b == data) {
+
+				admin.UseRegistration();
+				break;
+
+			}
+		case 3:
+			if (c == data) {
+
+				admin.UserHistory();
+				break;
+
+			}
+		}
+	}
 	   	  
-	   	  Admin admin=new Admin();
-		
-		Scanner sc=new Scanner(System.in);
-		
-		System.out.println("Enter the Required Details:Product_Details or User_Details ");
-		
-		  String Data=sc.next();
-		  String s="Product_Details";
-		  String r="User_Details";
-		  
-		if(s.equals(Data)) {
-			admin.ProductDetails();
-			
-		}
-		else if(r.equals(Data)) {
-			
-			admin.UseRegistration();
-			
-		
-			
-		}
-	   }
-	  
-
-}
-
-
+	   	
--- E-commerce/src/com/databases/StockDetails.java
+++ E-commerce/src/com/databases/StockDetails.java
@@ -9,28 +9,29 @@ import java.sql.Statement;
 
 public class StockDetails {
 
-	public void getStockUpdate(int purchaseQty, int Product_id) {
+	public void getStockUpdate(int purchaseQty, int Product_id) {       //Method is Createdfor getting the stock details by taking input
 		PreparedStatement ps = null;
 		PreparedStatement ps1 = null;
 		
-		int b=0;
+		int b=0;          //creating local variable for storing the updated stock quantity basesd on entered Product_id
 
 		try {
-			ConnectionUtility obj=new ConnectionUtility();
+			ConnectionUtility obj=new ConnectionUtility();        //Accessing connection method from outside
 			Connection con=obj.getConnection();
-			ps = con.prepareStatement("Select Quantity from products where Product_id=" + Product_id);
+			ps = con.prepareStatement("Select Quantity from products where Product_id=" + Product_id);  
+                       //select query is used to get product quauntity based on the product id entered
 
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
 
-				 b =(rs.getInt("Quantity")- purchaseQty);
+				 b =(rs.getInt("Quantity")- purchaseQty);        //storing the updated stock into the local var b
 				 //System.out.println(b);
 				
 			}
 			
-     		ps1= con.prepareStatement("update products set Quantity=? where Product_id=?");
+     		ps1= con.prepareStatement("update products set Quantity=? where Product_id=?");  //with the help of update query stock is updated for respectived product id
 			ps1.setInt(1, b);
-			ps1.setInt(2,Product_id);
+			ps1.setInt(2,Product_id);  //setting the quantity ,product id
 		      int i=ps1.executeUpdate();
 		      
 		  	//System.out.println("Step 5");
@@ -40,7 +41,7 @@ public class StockDetails {
 		      }
 	
 		catch (Exception e) {
-			e.printStackTrace();
+			e.printStackTrace();      //Exception is handled using try catch block
 		
 
 	}
