1aba63fc2389d303a6a4d3d0b12f81ff8fcc71cd
Zoro-Roronoaa <ani96aniket@gmail.com>
2022-07-18 17:02:57 +0530

    change

--- E-commerce/src/com/MAINM/CartDisplay.java
+++ E-commerce/src/com/MAINM/CartDisplay.java
@@ -11,30 +11,29 @@ import com.databases.ConnectionUtility;
 public class CartDisplay {
 
 	public void cartDisplay(String username) {
-		ConnectionUtility obj=new ConnectionUtility();
+		ConnectionUtility obj = new ConnectionUtility();
 		try {
-			Connection con=obj.getConnection();
-			PreparedStatement ps=con.prepareStatement("Select ProductName,Quantity from userhistory where username="+"'"+username+"'");
-			ResultSet rs=ps.executeQuery();
-			 System.out.println("|ProductName    |     Quantity|");//sop to print
-			 System.out.println("|-----------------------------|");
-			while(rs.next()) {
-System.out.println(rs.getString("ProductName"));
-     System.out.print("                    ");
-	     System.out.print(rs.getInt("Quantity"));
+			Connection con = obj.getConnection();
+			PreparedStatement ps = con.prepareStatement(
+					"Select ProductName,Quantity from userhistory where username=" + "'" + username + "'");
+			ResultSet rs = ps.executeQuery();
+			System.out.println("|ProductName    |     Quantity|");// sop to print
+			System.out.println("|-----------------------------|");
+			while (rs.next()) {
+				System.out.println(rs.getString("ProductName"));
+				System.out.print("                    ");
+				System.out.print(rs.getInt("Quantity"));
 				System.out.println("");
 			}
 			ps.execute();
 			rs.close();
 			ps.close();
 			con.close();
-			
-			
+
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 
 	}
 
